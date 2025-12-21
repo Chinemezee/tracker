a7000f9fcc680dbba596c51d642cb16aad62ee7f
Chinemezee <chinemezenjoku@gmail.com>
2025-12-21 23:06:11 +0100

    expense tracker first commit

---
+++ main.py
@@ -0,0 +1,65 @@
+from datetime import date
+from datetime import datetime
+
+expense_store = []
+
+def add_expense(category, amount, note, today, time):
+    dict = {
+        "category": category,
+        "amount": amount,
+        "note": note,
+        "date": today,
+        "time": time
+    }
+    expense_store.append(dict)
+    return expense_store
+
+def view_expense():
+     for items in expense_store:
+          print(f"{items['category']} | {items['amount']} | {items['note']} | {items['date']} {items['time']}")
+
+while True:
+    category = input('What type of product is it: ')
+    if category == "stop":
+         break
+    amount = int(input('Amount:')) 
+    note = input('Note: ')
+
+    today = date.today().strftime("%d-%m-%Y")
+    time = datetime.now().strftime("%H:%M:%S")
+
+    add_expense(category, amount, note, today, time)
+
+view_expense()
+
+sort_by = input("sort category by: ").lower()
+'''def filter_by_category():
+    found = False
+    for items in expense_store:
+        if(items['category'].lower() == sort_by):
+            print(f"{items['category']} | {items['amount']} | {items['note']} | {items['date']} {items['time']}")
+            found = True
+    if not found:
+        print("not found")  
+
+filter_by_category()'''
+
+def total_by_category():
+    total = 0
+    for items in expense_store:
+        if(items["category"].lower() == sort_by):
+            for item in expense_store:
+                total += item["amount"]
+    print(total)
+
+total_by_category
+
+'''def total_spending():
+    total = 0
+    for items in expense_store:
+        total += items["amount"]
+    print(f"this is the total of all the expenditure: {total}")
+
+total_spending()
+'''
+
