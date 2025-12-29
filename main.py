7ec53e1ba366f824e199af6c1b34572a350965be
Chinemezee <chinemezenjoku@gmail.com>
2025-12-29 23:58:20 +0100

    bug fixes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import json
     
 expense_store = []
 
-def add_expense(category, amount, note, today, time):
+def add_expense():
     dict = {
         "category": category,
         "amount": amount,
@@ -25,15 +25,21 @@ while True:
     category = input('What type of product is it: ')
     if category == "stop":
          break
-    amount = int(input('Amount:')) 
+    try:
+        amount = int(input('Amount:'))
+        if amount < 1:
+            print("enter a value above 0")
+            continue
+    except ValueError:
+        print("enter a valid number")
+        continue
     note = input('Note: ')
 
     today = date.today().strftime("%d-%m-%Y")
     time = datetime.now().strftime("%H:%M:%S")
 
-    add_expense(category, amount, note, today, time)
+    add_expense()
 
-#view_expense()
 
 def filter_by_category():
     sort_by = input("sort category by: ").lower()
@@ -44,7 +50,6 @@ def filter_by_category():
             found = True
     if not found:
         print("not found")  
-#filter_by_category()
 
 def total_by_category():
     sort_by = input("sort category by: ").lower()
@@ -58,14 +63,12 @@ def total_by_category():
         print("this catrgory was not found")
     else:
         print(f"the total of the {sort_by} category: {total}")
-#total_by_category()
 
 def total_spending():
     total = 0
     for items in expense_store:
         total += items["amount"]
     print(f"this is the total of all the expenditure: {total}")
-#total_spending()
 
 def load_expenses():
     try:
@@ -78,38 +81,39 @@ def load_expenses():
     except json.JSONDecodeError:
         print("file is corrupted or not valid json")
         expense_store = []
-#load_expenses() 
 
 def save_expenses():
     with open("expenses.json", "w") as file:
         json.dump(expense_store, file, indent=4)
     print("Expenses saved successfully.")
-#save_expenses()
 
 while True:
-    print("1. Add expense\n2. View all\n3. Filter by category\n4. Total spending\n5. Total by category\n6. Save expenses\n7. Load expenses\n8. Exit")
+    print("1. View all\n2. Filter by category\n3. Total spending\n4. Total by category\n5. Save expenses\n6. Load expenses\n7. Exit")
 
     menu_input = int(input("Choose your function number: ")) 
 
-    if menu_input == 1:
-        add_expense()
-    elif menu_input == 2:
-        view_expense()
-    elif menu_input == 3:
-        filter_by_category()
-    elif menu_input == 4:
-        total_spending()
-    elif menu_input == 5:
-        total_by_category()
-    elif menu_input == 6:
-        save_expenses()
-    elif menu_input == 7:
-        load_expenses()
-    elif menu_input == 8: 
-        break
-    else: 
-        print("invalid command")
-        continue
+    try:
+        if menu_input == 1:
+            view_expense()
+        elif menu_input == 2:
+            filter_by_category()
+        elif menu_input == 3:
+            total_spending()
+        elif menu_input == 4:
+            total_by_category()
+        elif menu_input == 5:
+            save_expenses()
+        elif menu_input == 6:
+            load_expenses()
+        #elif menu_input == 8:
+            #add_expense()
+        elif menu_input == 7: 
+            break
+        else: 
+            print("invalid command")
+            continue
+    except ValueError:
+        print("the input has to be a number")
    
 
 
